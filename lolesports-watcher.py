2f2226f44f1d7d64a0f2efc16123f30d2caeb723
FT <21099480+Bla@users.noreply.github.com>
2020-09-26 09:40:10 +0200

    Fix timing issues

--- lolesports-watcher.py
+++ lolesports-watcher.py
@@ -18,7 +18,7 @@ WORLDS_VOD_URL = "https://lolesports.com/vods/worlds/worlds_2020"
 # LCK_VOD_URL = "https://watch.lolesports.com/vods/lck/lck-summer-2020"
 # LPL_VOD_URL = "https://watch.lolesports.com/vods/lpl/lpl-summer-2020"
 REGEX = r"vod/\d{18}/\d" # RegEx for URL pattern, e.g.: /vod/123456789012345678/1
-NUMBER_OF_GAMES = 15 # Set the number of games to watch
+NUMBER_OF_GAMES = 1 # Set the number of games to watch
 MUTE_AUDIO = True 
 
 # Open a Chrome browser window
@@ -63,15 +63,15 @@ watch_time_in_minutes = 13 # Should watch long enough for it to count towards mi
 youtube_frame = '//iframe[starts-with(@src, "https://www.youtube.com/embed")]'
 for url in url_list:
     driver.get(url)
-    WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, youtube_frame)))
+    WebDriverWait(driver, 20).until(ec.visibility_of_element_located((By.XPATH, youtube_frame)))
     driver.switch_to.frame(driver.find_element_by_xpath(youtube_frame))
-    WebDriverWait(driver, 10).until(ec.element_to_be_clickable((By.XPATH, '//button[@aria-label="Play (k)"]'))).click()
+    WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.XPATH, '//button[@aria-label="Play (k)"]'))).click()
     print("Now watching " + url)
     time.sleep(watch_time_in_minutes * 60)
     print("Done!\n")
     watch_counter += 1
     if watch_counter == NUMBER_OF_GAMES:
-        hover_element = driver.find_element_by_id("riotbar-account");
+        hover_element = driver.find_element_by_id("riotbar-account-bar")
         action = ActionChains(driver)
         action.move_to_element(hover_element).perform()
         driver.find_element_by_xpath("//*[@data-riotbar-account-action='logout']").click()
